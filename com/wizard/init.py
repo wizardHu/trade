2186d44098c98595c7322b9ad1166cd5efc09acc
hulujie <hulujie@172.28.36.57>
2018-02-06 20:39:43 +0800

    KDJ

--- com/wizard/init.py
+++ com/wizard/init.py
@@ -8,7 +8,6 @@ def get_MA(datas,count):
     x = []
     y = []
     xy = []
-    
         
     for data in datas:
         index = datas.index(data)
@@ -48,9 +47,58 @@ def get_kline_xy(data):
     return xy
 
 
+def get_KDJ(data):
+    KDJ = []
+    KXY = []
+    DXY = []
+    JXY = []
+    
+    x = []
+    ky = []
+    dy = []
+    jy = []
+    
+    lastK = 50
+    lastD = 50
+    
+    for i in data:
+        Cn = i['close']
+        Ln = i['low']
+        Hn = i['high']
+        RSV = (Cn-Ln)/(Hn-Ln)*100 
+        
+        K = 2.0/3*lastK+1.0/3*RSV
+        D = 2.0/3*lastD+1.0/3*K
+        J = 3*K-2*D        
+         
+        x.append(data.index(i))
+        ky.append(K)
+        dy.append(D)
+        jy.append(J)
+        
+        lastK = K
+        lastD = D
+    
+    KXY.append(x)
+    KXY.append(ky)
+    
+    DXY.append(x)
+    DXY.append(dy)
+    
+    JXY.append(x)
+    JXY.append(jy)
+    
+    KDJ.append(KXY)
+    KDJ.append(DXY)
+    KDJ.append(JXY)
+    
+    return KDJ
+
+
 if __name__ == '__main__':
     
-    test = huobi.get_kline('xrpusdt','1min',1200)
+    fig = plt.figure()
+    test = huobi.get_kline('btcusdt','1day',1200)
     test['data'].reverse()
   
     klineXY = get_kline_xy(test['data'])
@@ -61,12 +109,21 @@ if __name__ == '__main__':
     MA30XY = get_MA(test['data'],30)
     MA10XY = get_MA(test['data'],10)
 
-#     plt.text(tempx, tempy, "sss %s %s" % (tempx,tempy))  
-#     plt.plot(tempx,tempy,'ro',color='red' )
-    plt.plot(klinex, kliney, label='xrpusdt')
-    plt.plot(MA60XY[0], MA60XY[1], label='ma60')
-    plt.plot(MA30XY[0], MA30XY[1], label='ma30')
-    plt.plot(MA10XY[0], MA10XY[1], label='ma10')
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+
+    ax1.plot(klinex, kliney, label='xrpusdt')
+    ax1.plot(MA60XY[0], MA60XY[1], label='ma60')
+    ax1.plot(MA30XY[0], MA30XY[1], label='ma30')
+    ax1.plot(MA10XY[0], MA10XY[1], label='ma10')
    
-    plt.legend()
+    KDJ = get_KDJ(test['data'])
+    ax2.plot(KDJ[0][0], KDJ[0][1], color="red")
+    ax2.plot(KDJ[1][0], KDJ[1][1], color='blue')
+    ax2.plot(KDJ[2][0], KDJ[2][1], color='green')
+   
+   
     plt.show()
+    
+    
+    
