aa84c17277251a022bc56f355001944ecaed777b
hulujie <741802012@qq.com>
2019-04-24 18:30:21 +0800

    卖

---
+++ com/sell/KDJ.py
@@ -0,0 +1,71 @@
+import Point as point
+
+lastK = [50]
+lastD = [50]
+lastJ = [50]
+
+def calKDJ(data,index):
+    global lastK
+    global lastD
+    global lastJ
+
+    Cn = data['close']
+    Ln = data['low']
+    Hn = data['high']
+
+    if Ln == Hn:
+        lastK.append(lastK[-1])
+        lastD.append(lastD[-1])
+        lastJ.append(lastJ[-1])
+        return
+
+    RSV = (Cn - Ln) / (Hn - Ln) * 100
+    K = 2.0 / 3 * lastK[-1] + 1.0 / 3 * RSV
+    D = 2.0 / 3 * lastD[-1] + 1.0 / 3 * K
+    J = 3 * K - 2 * D
+
+    lastK.append(K)
+    lastD.append(D)
+    lastJ.append(J)
+
+    if len(lastK)>10:
+        lastK = lastK[1:]
+        lastD = lastD[1:]
+        lastJ = lastJ[1:]
+
+def getLastK():
+    global lastK
+    return lastK[-1]
+
+def getLastD():
+    global lastD
+    return lastD[-1]
+
+def getLastJ():
+    global lastJ
+    return lastJ[-1]
+
+def judgeSell():
+    global lastK
+    global lastD
+    global lastJ
+
+    #当前的K值
+    ck = lastK[-1]
+    cd = lastD[-1]
+
+    #上一个的K值
+    lk = lastK[-2]
+    ld = lastD[-2]
+
+    p1 = point.Point(1, lk)
+    p2 = point.Point(2, ck)
+    line1 = point.Line(p1, p2)
+
+    p3 = point.Point(1, ld)
+    p4 = point.Point(2, cd)
+    line2 = point.Line(p3, p4)
+
+    pointXY = point.GetCrossPoint(line1, line2)
+    if pointXY.y < 60:
+        isBuy = True
---
+++ com/sell/plotAnalyse.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import HuobiService as huobi
+import matplotlib.pyplot as plt
+from matplotlib.ticker import  MultipleLocator
+import KDJ as KDJ
+from matplotlib.ticker import  FormatStrFormatter
+import globalUtil as constant
+import Client as client
+import time
+
+if __name__ == '__main__':
+
+    fig = plt.figure()
+    symbols = 'eosusdt'
+    test = huobi.get_kline(symbols, '1min', 1000)
+
+    test['data'].reverse()
+
+    dataList = []
+    dataListX = []
+
+#用于画图
+    K = []
+    D = []
+    J = []
+
+    for data in test['data']:
+        index = test['data'].index(data)
+        dataList.append(data['close'])
+        dataListX.append(index)
+
+        KDJ.calKDJ(data,index)
+        K.append(KDJ.getLastK())
+        D.append(KDJ.getLastD())
+        J.append(KDJ.getLastJ())
+
+    xmajorLocator = MultipleLocator(50);
+
+    #三行一列 获取第 1 行 第 0 列的图表，占一列，两行
+    ax1 = plt.subplot2grid((3, 1), (0, 0), colspan = 1, rowspan = 2)
+
+    # 三行一列 获取第 3 行 第 0 列的图表，占一列，一行
+    ax2 = plt.subplot2grid((3, 1), (2, 0), colspan = 1, rowspan = 1)
+    # ax3 = fig.add_subplot(313)
+
+    ax1.plot(dataListX,dataList)
+    ax1.xaxis.set_major_locator(xmajorLocator)
+    ax1.grid(linestyle='--')
+
+    ax2.plot(dataListX,K, color="red")
+    ax2.plot(dataListX,D, color='blue')
+    # ax2.plot(dataListX,J, color='green')
+    ax2.xaxis.set_major_locator(xmajorLocator)
+    ax2.grid(linestyle='--')
+
+    # ax3.plot(dataListX, dataList, label=symbols)
+    # ax3.xaxis.set_major_locator(xmajorLocator)
+    # ax3.grid(linestyle='--')
+
+    plt.show()
