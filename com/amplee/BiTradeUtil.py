081ed36a525d780e8d6a50f01c83d5e7eef63de0
hulujie <741802012@qq.com>
2019-07-19 19:20:10 +0800

    加个判断

--- com/amplee/BiTradeUtil.py
+++ com/amplee/BiTradeUtil.py
@@ -10,7 +10,7 @@ def buy(env,buyPrice,amount,symbol,index):
         orderId = "0000"
         if "pro" == env:
             result = huobi.send_order(amount, "api", symbol, "buy-limit", buyPrice)
-            logUtil.info("buy result",result)
+            logUtil.info("buy result",result,symbol,amount,buyPrice)
             if result['status'] == 'ok':
                 orderId = result['data']
             else:
@@ -32,6 +32,8 @@ def sell(env,sellPrice,sellIndex,buyModel,symbol):
             state = data['state']
             if state == 'filled':
                 result = huobi.send_order(buyModel.amount, "api", symbol, 'sell-limit', sellPrice)
+            else:
+                return
 
         fileOperUtil.delMsgFromFile(buyModel,"buy/"+symbol+"buy")
         fileOperUtil.write(('0,{},{},{},{},{},{}').format(int(time.time()),buyModel.price, buyModel.amount,sellPrice,sellIndex,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(sellIndex))),"record/"+symbol+"-record")
